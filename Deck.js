f27e8b3ee01c0aeac7744e3059c27ca91a945bd8
Patrick Sontag <pmsontag84@gmail.com>
2022-11-03 12:14:01 -0500

    Deck class, newDeck

--- Deck.js
+++ Deck.js
@@ -2,26 +2,22 @@
 
 class Deck {
     constructor() {
-        this.deck = this.newDeck();
+        this.deck = '';
         this.currentCard = '';
         this.nextCard = '';
         this.cardImage = '';
+        
+        this.newDeck();
     }
-    newDeck() {
-       axios
-            .get(`${baseCardsURL}/deck/new/shuffle/?deck_count=1`)
-            .then(d => {
-                deck = d.data.deck_id;
-                this.deck = deck;
-                return axios.get(`${baseCardsURL}/deck/${deck}/draw/?count=1`)
-            .then(c => {
-                this.nextCard = c.data.cards[0].code;
-                // let card = c.data.cards[0].code;
-                // this.nextCard = card;
-                this.cardImage = c.data.cards[0].image;
-            })
-        })
+    async newDeck() {
+       let res = await axios.get(`${baseCardsURL}/deck/new/shuffle/?deck_count=1`)
+       this.deck = res.data.deck_id;
+       let cardRes = await axios.get(`${baseCardsURL}/deck/${this.deck}/draw/?count=1`)
+       this.nextCard = cardRes.data.cards[0].code;
+       this.cardImage = cardRes.data.cards[0].image;
+       console.log("newDeck", "nextCard:", this.nextCard, "cardImage", this.cardImage);
     }
+
     newCard() {
         console.log("NEXT card", this.nextCard);
         this.currentCard = this.nextCard;
@@ -40,11 +36,3 @@ class Deck {
         return this.currentCard;
     }
 }
-
-// .get(`${baseCardsURL}/deck/new/shuffle/?deck_count=1`)
-
-// .then(d2 => {
-//     console.log(d2.data.deck_id);
-//     deck = d2.data.deck_id;
-//     return axios.get(`${baseCardsURL}/deck/${deck}/draw/?count=1`)
-// })
